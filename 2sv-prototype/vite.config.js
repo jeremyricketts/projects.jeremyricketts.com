ba138143e300d438577b42398fea4527396bb638
Jeremy Ricketts <jeremy@planningcenter.com>
2025-08-21 18:45:32 -0700

    Added base dir

--- 2sv-prototype/vite.config.js
+++ 2sv-prototype/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2sv-prototype/',
   preview: {
     allowedHosts: ['.ngrok-free.app']
   }
